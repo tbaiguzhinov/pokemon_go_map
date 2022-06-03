1cacefd59aef3ebb118fbd51f9ee853a06a58481
Baiguzhinov <ALABAIGUT@CORP.JTI.COM>
2022-06-03 17:36:45 +0600

    show pokemons on mainpage map

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,10 +1,11 @@
 from email.base64mime import header_length
+from email.policy import default
 from tkinter import CASCADE
 from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to="pokemon_images", blank=True)
+    image = models.ImageField(upload_to="pokemon_images", default=None, blank=True)
 
     def __str__(self):
         return self.title
@@ -13,13 +14,15 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=None)
-    disappeared_at = models.DateTimeField(default=None)
-    level = models.IntegerField(null=True)
-    health = models.IntegerField(null=True)
-    strength = models.IntegerField(null=True)
-    defence = models.IntegerField(null=True)
-    stamina = models.IntegerField(null=True)
+    appeared_at = models.DateTimeField(null=True, blank=True)
+    disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(null=True, default=None, blank=True)
+    health = models.IntegerField(null=True, default=None, blank=True)
+    strength = models.IntegerField(null=True, default=None, blank=True)
+    defence = models.IntegerField(null=True, default=None, blank=True)
+    stamina = models.IntegerField(null=True, default=None, blank=True)
+    def __str__(self):
+        return f"{self.pokemon} {self.level}"
 
     class Meta:
         verbose_name_plural = "PokemonEntities"
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from pokemon_entities.models import Pokemon
+from pokemon_entities.models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -27,26 +27,24 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    pokemon_entities = PokemonEntity.objects.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+    for pokemon_entity in pokemon_entities:
+        add_pokemon(
+            folium_map, pokemon_entity.lat,
+            pokemon_entity.lon,
+            request.build_absolute_uri(pokemon_entity.pokemon.image.url)
             )
 
     pokemons = Pokemon.objects.all()
-    print(pokemons)
+
     pokemons_on_page = []
     for pokemon in pokemons:
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
             'title_ru': pokemon.title,
-            'img_url': pokemon.image.url if pokemon.image else "",
+            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else "",
         })
 
     return render(request, 'mainpage.html', context={
