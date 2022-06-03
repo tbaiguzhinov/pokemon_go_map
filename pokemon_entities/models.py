8ed865b1b792b6ad869bc7d5af40662eaad82ead
Baiguzhinov <ALABAIGUT@CORP.JTI.COM>
2022-06-03 15:47:52 +0600

    show pokemons on mainpage

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to="pokemon_images", null=True)
+    image = models.ImageField(upload_to="pokemon_images", blank=True)
 
     def __str__(self):
         return self.title
